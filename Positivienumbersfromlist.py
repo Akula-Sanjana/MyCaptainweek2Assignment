b932a5bb8bfea3dde58cfc9c0b4495f2ccd22e96
Akula-Sanjana <146711990+Akula-Sanjana@users.noreply.github.com>
2023-10-20 12:16:24 +0530

    Add files via upload

---
+++ Positivienumbersfromlist.py
@@ -0,0 +1,14 @@
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+
+# Printing positive numbers from list1
+print("list1")
+for i in list1:
+    if i >= 0:
+        print(i, end=" ")
+
+# Printing positive numbers from list2
+print("\nlist2")
+for j in list2:
+    if j >= 0:
+        print(j, end=" ")
